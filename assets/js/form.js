c291a231166ab90704e2bed293d079d0e9e2850f
Bcz25 <160162513+Bcz25@users.noreply.github.com>
2024-03-24 00:18:56 +0000

    updated js

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,4 +5,8 @@ const submit = document.getElementsByClassName('submit')
 toggle.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode')
     
+    const elementsToToggle = document.getElementsByClassName('dark-mode');
+        for (let i = 0; i < elementsToToggle.length; i++) {
+    elementsToToggle[i].classList.toggle('.dark-mode');
+  }
 });

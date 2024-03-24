c5bc10f58539a8f94c0b721e4ba66ac0eb7be230
Bcz25 <160162513+Bcz25@users.noreply.github.com>
2024-03-24 02:42:16 +0000

    added js to submit button

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,7 +2,9 @@ const modeToggle = document.getElementsByClassName('toggle')[0];
 const landing = document.getElementsByClassName('landing') [0];
 const card = document.getElementsByClassName('blog-card')[0];
 const submit = document.getElementById('submit')
-
+const username = document.getElementById('usersame').value;
+const title = document.getElementById('title').value;
+const content = document.getElementById('content').value;
 
 modeToggle.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
@@ -15,3 +17,21 @@ modeToggle.addEventListener('click', function() {
     }
 });
 
+
+username.addEventListener('input', function (){
+    localStorage.setItem('username', this.value);
+})
+
+title.addEventListener('input', function (){
+    localStorage.setItem('title', this.value);
+})
+
+content.addEventListener('input', function (){
+    localStorage.setItem('content', this.value);
+})
+
+submit.addEventListener('click', function (){
+    localStorage.getItem('username')
+    localStorage.getItem('title');
+    localStorage.getItem('content');
+})

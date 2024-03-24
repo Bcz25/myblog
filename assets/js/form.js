bfb5310e7f4e2fdc741ba1418a21f04fb9fb5afd
Bcz25 <160162513+Bcz25@users.noreply.github.com>
2024-03-24 00:42:41 +0000

    updated dark mode

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,9 @@
-const modeToggle = document.getElementsByClassName('toggle');
+const modeToggle = document.getElementsByClassName('toggle')[0];
 const submit = document.getElementsByClassName('submit')
 
 
 modeToggle.addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode')
+    document.body.classList.toggle('dark-mode');
     
     const elementsToToggle = document.getElementsByClassName('dark-mode');
         for (let i = 0; i < elementsToToggle.length; i++) {

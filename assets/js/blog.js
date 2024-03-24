2da4ebaf5bfc74d7ae6ded08a1dd4feab3c883da
Bcz25 <160162513+Bcz25@users.noreply.github.com>
2024-03-24 23:52:42 +0000

    changed header styling blog.css

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,9 @@
 const modeToggle = document.getElementsByClassName('toggle')[0];
+const blogHeader= document.getElementById('blog-header')
 
 modeToggle.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
-})
+    if (blogHeader) {
+        blogHeader.classList.toggle('dark-mode');
+    }
+});

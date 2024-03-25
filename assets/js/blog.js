f912070947f38e40293fd7a8c0bab7e15d4732f4
Bcz25 <160162513+Bcz25@users.noreply.github.com>
2024-03-25 00:12:24 +0000

    added function for back button

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,6 @@
 const modeToggle = document.getElementsByClassName('toggle')[0];
-const blogHeader= document.getElementById('blog-header')
+const blogHeader = document.getElementById('blog-header')
+const back = document.getElementById('back')
 
 modeToggle.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
@@ -7,3 +8,7 @@ modeToggle.addEventListener('click', function() {
         blogHeader.classList.toggle('dark-mode');
     }
 });
+
+back.addEventListener('click', function() {
+    window.location.href="https://bcz25.github.io/myblog"
+})

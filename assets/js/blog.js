fe0fbb6d2b66503c7ca8a56c2a3aeb53218c31a0
Bcz25 <160162513+Bcz25@users.noreply.github.com>
2024-03-25 03:10:04 +0000

    added posts to blog page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,16 @@
 const modeToggle = document.getElementsByClassName('toggle')[0];
-const blogHeader = document.getElementById('blog-header')
-const back = document.getElementById('back')
+const blogHeader = document.getElementById('blog-header');
+const back = document.getElementById('back');
+const postList= document.getElementById('post-list');
+const post = JSON.parse(localStorage.getItem('post')) || [];
+
+
+post.forEach(function(blogPost) {
+    const li = document.createElement('li');
+    li.textContent = blogPost;
+    postList.appendChild(li);
+});
+
 
 modeToggle.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
@@ -18,3 +28,4 @@ modeToggle.addEventListener('click', function() {
 back.addEventListener('click', function() {
     window.location.href="https://bcz25.github.io/myblog"
 })
+
